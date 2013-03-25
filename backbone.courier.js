709668391a9989d317efb04d86cd93f048986fa2
Oleg Seletsky <oleg.b.seletsky@gmail.com>
2013-03-25 10:44:49 -0700

    Increase version number to 0.5.6

--- backbone.courier.js
+++ backbone.courier.js
@@ -1,5 +1,5 @@
 /*!
- * Backbone.Marionette.Courier, v0.5.5
+ * Backbone.Marionette.Courier, v0.5.6
  * Copyright (c)2013 Rotunda Software, LLC.
  * Distributed under MIT license
  * http://github.com/rotundasoftware/backbone.marionette.courier
